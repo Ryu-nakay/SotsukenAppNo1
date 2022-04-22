e82bc63c0e3ba798e1563b58b42c4e5ecda561f8
Ryu-nakay-from_MBA <70783066+Ryu-nakay@users.noreply.github.com>
2022-04-22 12:21:32 +0900

    Initial Commit

---
+++ SotsukenAppNo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SotsukenAppNo1
+//
+//  Created by 中山龍 on 2022/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SotsukenAppNo1/SotsukenAppNo1App.swift
@@ -0,0 +1,17 @@
+//
+//  SotsukenAppNo1App.swift
+//  SotsukenAppNo1
+//
+//  Created by 中山龍 on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SotsukenAppNo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
